58e70ae8a28b8cb9af1c4e95de6504f17083197b
banglh <ice.banglh@gmail.com>
2014-08-25 15:21:35 +0900

    add GameEngine class

---
+++ expGame/GameEngine.java
@@ -0,0 +1,204 @@
+package expGame;
+
+import java.util.Arrays;
+import java.util.Random;
+
+
+public class GameEngine {
+	// constants
+	public static final int NORTH = 0;	// north direction
+	public static final int EAST = 1;	// east direction
+	
+	// game parameters
+	private double [][] map;		// game board
+	private int s;					// board size
+	private double chi, d, p;		// chi, d, p params
+	private int nf;					// number of radial basis functions
+	private int [][] RBFsPos;		// positions of radial basis function centers
+	private double sdRBF; 			// standard deviation of radial basis functions
+	private int [] agentInitPos;	// initial agent's position
+	private boolean initialized = false;
+	private Random rand;
+
+	// game status
+	private int [] agentCurPos;		// current position of agent
+	private double totalReward;		// current total reward
+	private boolean gameEnded = false;		// game ended or not
+
+	// constructors
+	public GameEngine(int s, double chi, double d, double p) {
+		// set params
+		this.s = s;
+		this.chi = chi;
+		this.d = d;
+		this.p = p;
+		this.sdRBF = 1.0;
+		this.map = new double [s][s];
+		this.nf = (int) Math.floor(chi*(s-1)*(s-1));
+		this.RBFsPos = new int [this.nf][2];
+		this.agentInitPos = new int [2];
+		this.agentCurPos = new int [2];
+		rand = new Random(System.currentTimeMillis());
+	}
+	
+	// initialize game
+	public void initGame() {
+		// initialize map
+		initMap();
+		
+		// initialize RBF centers
+		initRBFs();
+		
+		// initialize agent position
+		initAgentPos();
+
+		agentCurPos = agentInitPos.clone();
+		totalReward = 0.0;
+		initialized = true;
+	}
+	
+	// initialize map
+	private void initMap() {
+		for (int row = 0; row < s; row++) {
+			for (int col = 0; col < s; col++) {
+				map[row][col] = rand.nextDouble();
+			}
+		}
+	}
+	
+	// RBF centers init
+	private void initRBFs() {
+		int maxNum = (s-1)*(s-1);
+		int [] positions = new int [maxNum];
+		int [] rbfs = new int [nf];
+		
+		// init positions
+		for (int i = 0; i < maxNum; i++) {
+			positions[i] = i;
+		}
+		
+		// randomly choose nf positions
+		for (int i = 0; i < nf; i++) {
+			// choose a random position from positions[i] ~ positions[maxNum-1]
+			int newPos = rand.nextInt(maxNum - i) + i;
+			
+			// exchange positions[i] with positions[newPos]
+			int tmp = positions[i];
+			positions[i] = positions[newPos];
+			positions[newPos] = tmp;
+			
+			rbfs[i] = positions[i];
+		}
+		
+		// sorting rbfs
+		Arrays.sort(rbfs);
+		
+		// convert positions
+		for (int i = 0; i < nf; i++) {
+			this.RBFsPos[i][0] = rbfs[i] / (s - 1) + 1;
+			this.RBFsPos[i][1] = rbfs[i] % (s - 1);
+		}
+	}
+	
+	// initialize agent's position
+	private void initAgentPos() {
+		int row = rand.nextInt(s - 1) + 1;		// from 1 to s-1
+		int col = rand.nextInt(s - 1);			// from 0 to s-2
+		agentInitPos[0] = row;
+		agentInitPos[1] = col;	
+	}
+
+	// get functions
+	public int get_s() {
+		return s;
+	}
+	
+	public double get_chi() {
+		return chi;
+	}
+	
+	public double get_d() {
+		return d;
+	}
+	
+	public double get_p() {
+		return p;
+	}
+	
+	public double [][] getMap() {
+		return map.clone();
+	}
+	
+	public int [][] getRBFsPos() {
+		return RBFsPos.clone();
+	}
+	
+	public int [] getAgentInitPos() {
+		return agentInitPos.clone();
+	}
+	
+	public int [] getAgentCurPos() {
+		return agentCurPos.clone();
+	}
+	
+	public double getTotalReward() {
+		return totalReward;
+	}
+	
+	public boolean isGameEnded() {
+		return gameEnded;
+	}
+
+	public boolean isGameInitialized() {
+		return initialized;
+	}
+
+	// print game info
+	public void printGameInfo() {
+		System.out.println("******* GAME INFORMATION *******");
+		// params
+		System.out.printf("+ s = %d\n", s);
+		System.out.printf("+ chi = %f\n", chi);
+		System.out.printf("+ d = %f\n", d);
+		System.out.printf("+ p = %f\n", p);
+		System.out.printf("+ nf = %d\n", nf);
+		System.out.printf("+ RBFs sd = %f\n", sdRBF);
+		System.out.printf("+ total reward = %f\n", totalReward);
+		System.out.printf("+ initialized = %b\n", initialized);
+		System.out.printf("+ game ended = %b\n\n", gameEnded);
+		
+		// map
+		System.out.println("+ Game Map");
+		for (int row = 0; row < s; row++) {
+			for (int col = 0; col < s; col++) {
+				System.out.printf("%-6.3f", map[row][col]);
+			}
+			System.out.println("");
+		}
+		
+		// RBFs pos
+		System.out.println("+ RBFs center positions");
+		for (int i = 0; i < nf; i++) {
+			System.out.printf("[%d][%d]\n", RBFsPos[i][0], RBFsPos[i][1]);
+		}
+		
+		// Agent's initial position
+		System.out.println("+ Agent's initial location");
+		System.out.printf("[%d][%d]\n", agentInitPos[0], agentInitPos[1]);
+		
+		// Agent's current position
+		System.out.println("+ Agent's current location");
+		System.out.printf("[%d][%d]\n", agentCurPos[0], agentCurPos[1]);
+	}
+	// main function
+	public static void main(String args[]) {
+		int s = 5;
+		double chi = 0.4;
+		double d = 0.1;
+		double p = 0.2;
+		GameEngine ge = new GameEngine(s, chi, d, p);
+		ge.printGameInfo();
+		ge.initGame();
+		ge.printGameInfo();
+	}
+}

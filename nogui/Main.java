925b5b0ae4cd72b0a674836f69dab93c162a1343
banglh <ice.banglh@gmail.com>
2014-08-27 22:05:33 +0900

    finish untested noGui version

--- nogui/Main.java
+++ nogui/Main.java
@@ -8,24 +8,45 @@ public class Main {
 	private Parameter a_parameter;
 	private Session a_session;
 	private Generation a_generation;
-	private Grafi a_grafi;
 	protected HistoryLog logger;
 	
 	public static void main(String [] args) {
-		
+		Main engine = new Main();
+		engine.run();
 	}
 	
 	public Main() {
-		a_parameter = new Parameter(_f);
-		a_session = new Session(_f);
-		a_generation = new Generation(_f);
-		a_grafi = new Grafi(_f);
+		a_parameter = new Parameter();
+		a_session = new Session();
+		a_generation = new Generation();
 		
 		logger = new HistoryLog();
 
 		a_parameter.setLog(logger);
 		a_session.setLog(logger);
 		a_generation.setLog(logger);
-		a_grafi.setLog(logger);
+	}
+	
+	public void reset() {
+		a_parameter = new Parameter();
+		a_session = new Session();
+		a_generation = new Generation();
+		
+		logger = new HistoryLog();
+
+		a_parameter.setLog(logger);
+		a_session.setLog(logger);
+		a_generation.setLog(logger);
+	}
+	
+	public void run() {
+		// load parameters
+		a_parameter.loadDefault();
+		
+		// load session setting
+		a_session.loadSessDefault();
+		
+		// run neat
+		a_generation.start();
 	}
 }
